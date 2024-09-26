1eb2446b6c9f82276b95773300e4c4bf4795f67a
Martim Carneiro Pacheco Nunes Pinto <mcarneir@c2r4s5.42porto.com>
2024-09-26 14:40:06 +0100

    Makefile

---
+++ Makefile
@@ -0,0 +1,35 @@
+NAME = inception
+SRCS = ./srcs
+COMPOSE = $(SRCS)/docker-compose.yml
+HOST_URL = mcarneir.42.fr
+DATA_DIR = /home/mcarneir/data
+
+all: conf up
+
+conf:
+	@sudo mkdir -p ${DATA_DIR}mariadb_volume/ ${DATA_DIR}wordpress_volume/
+	@sudo sed -i '/^127.0.0.1/ s/localhost/localhost achabrer.42.fr/' /etc/hosts || true
+
+up:
+	sudo docker compose -p ${NAME} --file ${COMPOSE} up --build -d
+
+down:
+	sudo docker compose -p ${NAME} down --volumes
+
+start:
+	sudo docker compose -p ${NAME} start
+
+stop:
+	sudo docker compose -p ${NAME} stop
+
+clean_images:
+	@echo "Removing images...\n"
+	sudo docker rmi -f $(sudo docker images -q) || true
+
+clean: down clean_images
+
+fclean: clean
+	@echo "Removing volumes...\n"
+	sudo rm -rf ${DATA_DIR}
+
+re: fclean all
--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -4,38 +4,50 @@ DB_NAME="inception_db"
 DB_USER="mcarneir"
 DB_PASS="trains123"
 DB_ROOT_PASS="42trains123"
-WP_URL="mcarneir.42.fr"
 WP_TITLE="webtitle"
 WP_ADMIN_USR="mcarneirpriv"
-WP_ADMIN_PASS="privtrains"
+WP_ADMIN_PWD="privtrains"
 WP_ADMIN_EMAIL="mcarneir@student.42porto.com"
+WP_URL="http://localhost"
 
-
+# Fix permissions
 chown -R www-data:www-data /var/www/*
 chmod -R 755 /var/www/*
 
+# Create PHP-FPM run directory and PID file
 mkdir -p /run/php/
 touch /run/php/php7.4-fpm.pid
 
+# Modify PHP-FPM to listen on port 9000 instead of a socket
 sed -i "s/listen = \/run\/php\/php7.4-fpm.sock/listen = 9000/" "/etc/php/7.4/fpm/pool.d/www.conf"
 
-if [ ! -f /var/www/html/wordpress.php ]; then
-		echo "Wordpress: setting up..."
+# If WordPress is not already set up, download and install
+if [ ! -f /var/www/html/wp-config.php ]; then
+    echo "Wordpress: setting up..."
 
-		mkdir -p /var/www/html
-		cd /var/www/html
-		rm -rf *
+    mkdir -p /var/www/html
+    cd /var/www/html
 
+    # Remove any existing files
+    rm -rf *
 
-R		wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
-		chmod +x wp-cli.phar 
-		mv wp-cli.phar /usr/local/bin/wp
-		wp core download --allow-root
+    # Download WP-CLI
+    wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+    if [ ! -f wp-cli.phar ]; then
+        echo "Failed to download wp-cli.phar"
+        exit 1
+    fi
 
-		sleep 2
+    chmod +x wp-cli.phar
+    mv wp-cli.phar /usr/local/bin/wp
 
-		wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb:3306
-		wp core install --url=$WP_URL/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+    # Download and configure WordPress
+    wp core download --allow-root
+    wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb:3306
+    wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+
+    echo "Wordpress: installation complete!"
 fi
 
+# Execute the CMD passed to the container (PHP-FPM)
 exec "$@"
