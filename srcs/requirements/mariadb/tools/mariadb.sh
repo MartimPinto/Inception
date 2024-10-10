cb0d45275b004ac7edf958287ef3f5a35da1e353
Martim Carneiro Pacheco Nunes Pinto <mcarneir@c1r4s7.42porto.com>
2024-10-10 15:10:49 +0100

    mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
-# Start MariaDB in the background
-mysqld_safe &
-sleep 5  # Give MariaDB some time to fully start
+service mariadb start
 
-# Prepare the SQL script to create the database and user
-cat >/tmp/db.sql << EOF
+mariadb -v -u root << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_PASS_ROOT';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_PASS_ROOT');
 EOF
 
-# Check if MariaDB is ready before running the SQL script
-until mysqladmin ping --silent; do
-    echo "Waiting for MariaDB to be ready..."
-    sleep 2
-done
+sleep 5
 
-# Execute the SQL script
-mysql -uroot -p"$DB_ROOT_PASS" < /tmp/db.sql
-if [ $? -eq 0 ]; then
-    echo "SQL script executed successfully."
-else
-    echo "Error executing SQL script."
-    exit 1
-fi
+service mariadb stop
 
+exec $@ 
 
 # Run the command passed as arguments to the script
