07cd63dfdefdc6b4db38cca59107f08b09c4773c
Martim Carneiro Pacheco Nunes Pinto <mcarneir@c1r4s7.42porto.com>
2024-10-10 14:46:15 +0100

    mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
-
+# Start MariaDB in the background
 mysqld_safe &
-sleep 5
+sleep 5  # Give MariaDB some time to fully start
 
-
+# Prepare the SQL script to create the database and user
 cat >/tmp/db.sql << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
@@ -14,14 +13,23 @@ ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
 FLUSH PRIVILEGES;
 EOF
 
-mysql -uroot </tmp/db.sql
+# Check if MariaDB is ready before running the SQL script
+until mysqladmin ping --silent; do
+    echo "Waiting for MariaDB to be ready..."
+    sleep 2
+done
+
+# Execute the SQL script
+mysql -uroot -p"$DB_ROOT_PASS" < /tmp/db.sql
 if [ $? -eq 0 ]; then
-	echo "SQL script executed successfully."
+    echo "SQL script executed successfully."
 else
-	echo "Error executing SQL script."
-	exit 1
+    echo "Error executing SQL script."
+    exit 1
 fi
 
+# Stop MariaDB
 service mariadb stop
 
+# Run the command passed as arguments to the script
 exec "$@"
