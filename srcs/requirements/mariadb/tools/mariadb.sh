24b9b06ff9a1154fd1d81f77d809260294a53792
Martim Carneiro Pacheco Nunes Pinto <mcarneir@c1r4s7.42porto.com>
2024-10-10 15:51:20 +0100

    mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -21,7 +21,7 @@ until mysqladmin ping --silent; do
 done
 
 # Execute the SQL script
-mysql -uroot -p"$DB_ROOT_PASS" < /tmp/db.sql
+mysql -uroot </tmp/db.sql
 if [ $? -eq 0 ]; then
     echo "SQL script executed successfully."
 else
