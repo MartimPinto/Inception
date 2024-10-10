aebc2692b058ecf9639a0898e0e7c408c5dd9856
Martim Carneiro Pacheco Nunes Pinto <mcarneir@c1r4s7.42porto.com>
2024-10-10 16:19:12 +0100

    mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
 # Ensure the data directory is initialized
-echo "Initializing MariaDB data directory..."
-mysql_install_db --user=mysql --ldata=/var/lib/mysql
-echo "MariaDB data directory initialized."
 
-# Start the MySQL server
-echo "Starting MySQL server..."
-mysqld_safe &
-sleep 5
+service mariadb start
 
-# Create a SQL script file and add SQL commands to it
-echo "Creating initial SQL script..."
-cat >/tmp/db.sql <<EOF
+mariadb -v -u root << EOF
 CREATE DATABASE IF NOT EXISTS $DB_NAME;
 CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO 'root'@'%' IDENTIFIED BY '$DB_ROOT_PASS';
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASS');
 EOF
 
-echo "Initial SQL script created. Executing script..."
-# Execute the SQL script using the MySQL client
-mysql -uroot </tmp/db.sql
-if [ $? -eq 0 ]; then
-	echo "SQL script executed successfully."
-else
-	echo "Error executing SQL script."
-	exit 1
-fi
+sleep 5
+
+service mariadb stop
+
+exec "$@" 
